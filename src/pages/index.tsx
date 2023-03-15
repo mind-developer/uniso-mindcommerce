1208cf3ebf67fb0c8b6ae59013f7a6166472cb97
GKyomen <kenkakyomen@hotmail.com>
2023-03-15 17:43:16 -0300

    :sparkles: Uso multiplo do produto em um grid

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,28 @@
 import Header from '@/components/Header'
+import Produto from '@/components/Produto'
+import { Grid, GridItem } from '@chakra-ui/react'
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <Grid templateColumns="repeat(4, 1fr)" m={4} gap={4}>
+        <GridItem>
+          <Produto />
+        </GridItem>
+        <GridItem>
+          <Produto />
+        </GridItem>
+        <GridItem>
+          <Produto />
+        </GridItem>
+        <GridItem>
+          <Produto />
+        </GridItem>
+        <GridItem>
+          <Produto />
+        </GridItem>
+      </Grid>
     </main>
   )
 }
