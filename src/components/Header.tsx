d69d50566d899baf7715b0da4c5c622c72e55dcc
GKyomen <kenkakyomen@hotmail.com>
2023-03-15 17:17:27 -0300

    :recycle: Extração do cabeçalho para componente

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import { Box, Button, Flex, Text } from '@chakra-ui/react'
+import { BsCart2 } from 'react-icons/bs'
+
+export default function Header() {
+  return (
+    <Box h="75px" px="5%" boxShadow="0 4px 5px rgba(0,0,0,0.5)">
+      <Flex h="100%" justify="space-between" align="center">
+        <Text color="red" fontWeight="bold" fontSize="3xl">
+          Mindcommerce
+        </Text>
+        <Button colorScheme="red" leftIcon={<BsCart2 />}>
+          Carrinho
+        </Button>
+      </Flex>
+    </Box>
+  )
+}
