40d5cbdf922fac9cfd32bf1b9c83ca393696bd07
GKyomen <kenkakyomen@hotmail.com>
2023-03-15 17:02:49 -0300

    :sparkles: Criação do cabeçalho

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,19 @@
+import { Box, Button, Flex, Text } from '@chakra-ui/react'
+import { BsCart2 } from 'react-icons/bs'
+
 export default function Home() {
-  return <></>
+  return (
+    <main>
+      <Box h="75px" px="5%" boxShadow="0 4px 5px rgba(0,0,0,0.5)">
+        <Flex h="100%" justify="space-between" align="center">
+          <Text color="red" fontWeight="bold" fontSize="3xl">
+            Mindcommerce
+          </Text>
+          <Button colorScheme="red" leftIcon={<BsCart2 />}>
+            Carrinho
+          </Button>
+        </Flex>
+      </Box>
+    </main>
+  )
 }
