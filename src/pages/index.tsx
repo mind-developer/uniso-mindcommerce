f36ea17f8b187b8d0941b095be0cb3e2abd326a0
GKyomen <kenkakyomen@hotmail.com>
2023-03-15 18:47:16 -0300

    :sparkles: Busca e uso dos dados da api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,45 @@
 import Header from '@/components/Header'
 import Produto from '@/components/Produto'
 import { Grid, GridItem } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+
+interface Produto {
+  id: number
+  title: string
+  description: string
+  category: string
+  image: string
+  price: number
+}
 
 export default function Home() {
+  const [produtos, setProdutos] = useState<Produto[]>([])
+
+  useEffect(() => {
+    async function buscaProdutos() {
+      const res = await fetch('https://fakestoreapi.com/products')
+      const dados: Produto[] = await res.json()
+
+      setProdutos(dados)
+    }
+
+    buscaProdutos()
+  }, [])
+
   return (
     <main>
       <Header />
       <Grid templateColumns="repeat(4, 1fr)" m={4} gap={4}>
-        <GridItem>
-          <Produto
-            linkImagem="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-            titulo="Sofá verde de 2 lugares"
-            descricao="Este sofá é perfeito para espaços tropicais modernos, espaços de inspiração barroca, espaços em tons de terra e para pessoas que adoram um design chique com uma pitada de design vintage."
-            preco={950}
-          />
-        </GridItem>
+        {produtos.map((item) => (
+          <GridItem key={item.id}>
+            <Produto
+              linkImagem={item.image}
+              titulo={item.title}
+              descricao={item.description}
+              preco={item.price}
+            />
+          </GridItem>
+        ))}
       </Grid>
     </main>
   )
