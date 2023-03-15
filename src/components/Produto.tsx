779fddffbd570f57c7db97578ea021f38ff371f1
GKyomen <kenkakyomen@hotmail.com>
2023-03-15 18:09:41 -0300

    :sparkles: Parametrização do componente de produto

--- src/components/Produto.tsx
+++ src/components/Produto.tsx
@@ -11,24 +11,28 @@ import {
   Text,
 } from '@chakra-ui/react'
 
-export default function Produto() {
+interface Props {
+  titulo: string
+  descricao: string
+  linkImagem: string
+  preco: number
+}
+
+export default function Produto({
+  titulo,
+  descricao,
+  linkImagem,
+  preco,
+}: Props) {
   return (
     <Card maxW="sm" boxShadow="4px 4px 5px rgba(0,0,0,0.5)">
       <CardBody>
-        <Image
-          src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-          alt="Sofá verde de 2 lugares"
-          borderRadius="lg"
-        />
+        <Image src={linkImagem} alt={titulo} borderRadius="lg" />
         <Stack mt="6" spacing="3">
-          <Heading size="md">Sofá verde de 2 lugares</Heading>
-          <Text>
-            Este sofá é perfeito para espaços tropicais modernos, espaços de
-            inspiração barroca, espaços em tons de terra e para pessoas que
-            adoram um design chique com uma pitada de design vintage.
-          </Text>
+          <Heading size="md">{titulo}</Heading>
+          <Text>{descricao}</Text>
           <Text color="red.600" fontSize="2xl" textAlign="end">
-            R$ 850,00
+            {`R$ ${preco}`}
           </Text>
         </Stack>
       </CardBody>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,19 +8,12 @@ export default function Home() {
       <Header />
       <Grid templateColumns="repeat(4, 1fr)" m={4} gap={4}>
         <GridItem>
-          <Produto />
-        </GridItem>
-        <GridItem>
-          <Produto />
-        </GridItem>
-        <GridItem>
-          <Produto />
-        </GridItem>
-        <GridItem>
-          <Produto />
-        </GridItem>
-        <GridItem>
-          <Produto />
+          <Produto
+            linkImagem="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+            titulo="Sofá verde de 2 lugares"
+            descricao="Este sofá é perfeito para espaços tropicais modernos, espaços de inspiração barroca, espaços em tons de terra e para pessoas que adoram um design chique com uma pitada de design vintage."
+            preco={950}
+          />
         </GridItem>
       </Grid>
     </main>
